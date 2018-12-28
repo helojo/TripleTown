ed3125696d2aa04cb70987d126262ffe4e3bc580
杨晓松 <xiaosongsz@foxmail.com>
2018-12-28 19:34:52 +0800

    修改常用库

--- assets/CleverCreator/Scripts/Core/CleverManager.ts
+++ assets/CleverCreator/Scripts/Core/CleverManager.ts
@@ -1,4 +1,3 @@
-import { Dictionary } from "typescript-collections";
 import BaseManager from "./BaseManager";
 import UIManager from "../UI/UIManager";
 
@@ -15,13 +14,14 @@ export default class CleverManager{
         return CleverManager.instance;
     }
 
-    protected managerMap:Dictionary<String, BaseManager> = new Dictionary<String, BaseManager>();
+    // protected managerMap:Dictionary<String, BaseManager> = new Dictionary<String, BaseManager>();
 
     private constructor(){
-        console.info("CleverManager.Name.", CleverManager.name);
-        
+        // console.info("CleverManager.Name.", CleverManager.name);
         //添加UIManager
-        this.managerMap.setValue(UIManager.name, new UIManager());
+        // this.managerMap.setValue(UIManager.name, new UIManager());
+
+        
     }
 
     /**
@@ -29,6 +29,6 @@ export default class CleverManager{
      * @param name 名字
      */
     public getManager(name:String){
-        return this.managerMap.getValue(name);
+        // return this.managerMap.getValue(name);
     }
 }
