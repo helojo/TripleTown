aa9c948d8bb42168defa072eb08e0e31bf521932
杨晓松 <xiaosongsz@foxmail.com>
2019-01-08 17:33:30 +0800

    关卡与逻辑的关系

--- assets/Game/Scripts/Game/Component/CGame.ts
+++ assets/Game/Scripts/Game/Component/CGame.ts
@@ -1,3 +1,5 @@
+import CLogic from "./Property/CLogic";
+import CLevel from "./Property/CLevel";
 
 const {ccclass, property} = cc._decorator;
 
@@ -7,4 +9,9 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class CGame extends cc.Component {
     
+    @property(CLevel)
+    protected level:CLevel = null;
+
+    @property(CLogic)
+    protected logic:CLogic = null;
 }
---
+++ assets/Game/Scripts/Game/Component/CLevel.ts
@@ -0,0 +1,9 @@
+const {ccclass, property} = cc._decorator;
+
+/**
+ * 关卡
+ */
+@ccclass
+export default class CLevel extends cc.Component {
+    
+}
