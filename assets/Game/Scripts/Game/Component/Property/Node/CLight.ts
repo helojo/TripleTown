47862d4c150f86750d0a7f446ad30cfbc9494f0d
杨晓松 <xiaosongsz@foxmail.com>
2019-01-10 13:52:07 +0800

    选中动画优化

--- assets/Game/Scripts/Game/Component/Property/Node/CLight.ts
+++ assets/Game/Scripts/Game/Component/Property/Node/CLight.ts
@@ -8,4 +8,11 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class CLight extends CNode {
     
+    protected onEnable(){
+        this.node.opacity = 0;
+        let aniCom = this.getComponent(cc.Animation);
+        aniCom.stop();
+        aniCom.play();
+    }
+
 }
