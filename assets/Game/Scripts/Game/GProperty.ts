d73ca839df9a8410ae60b35cbc0a2a411b6c25e6
杨晓松 <xiaosongsz@foxmail.com>
2019-01-11 11:32:02 +0800

    添加成员属性功能

--- assets/Game/Scripts/Game/GProperty.ts
+++ assets/Game/Scripts/Game/GProperty.ts
@@ -1,5 +1,21 @@
 import SDepend from "./Struct/SDepend";
 
 export default class GProperty {
+    protected children:Array<GProperty> = null;
+
+    /**
+     * 依赖
+     */
     public Depend:SDepend = null;
+
+    /**
+     * 成员属性
+     */
+    public get Children(){
+        return this.children;
+    }
+
+    public constructor (){
+        this.children = new Array<GProperty>();
+    }
 }
